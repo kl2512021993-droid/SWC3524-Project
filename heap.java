5e742df556980fa47d547c9ee0a19025c21e9737
kl2512021993-droid <kl2512021993@student.uptm.edu.my>
2026-03-30 09:25:58 +0800

    Update heap.java

--- heap.java
+++ heap.java
@@ -1,24 +1,28 @@
 import java.util.*;
-public class heap{
-// Min-Heap Initialization
-static class MinHeap{
-private PriorityQueue<Integer> heap = new PriorityQueue<>();
-public void insert(int value){
-heap.add(value);
-}
-public Integer getMin(){
-return heap.peek();
-}
-public Integer extractMin(){
-return heap.poll();
-}
-}
-public static void main(String[] args){
-// Min-Heap Test
-MinHeap heap = new MinHeap();
-heap.insert(30);
-heap.insert(26);
-heap.insert(15);
-System.out.println("Min-Heap Extract Min: " + heap.extractMin());
-}
+
+public class heap {
+    static class MinHeap {
+        private PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+        public void insert(int val) {
+            heap.add(val);
+        }
+
+        public Integer getMin() {
+            return heap.peek();
+        }
+
+        public Integer extractMin() {
+            return heap.poll();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Min-Heap Test
+        MinHeap heap = new MinHeap();
+        heap.insert(30);
+        heap.insert(26);
+        heap.insert(15);
+        System.out.println("Min-Heap Extract Min: " + heap.extractMin());
+    }
 }
